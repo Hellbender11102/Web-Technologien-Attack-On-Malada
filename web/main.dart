09168ccbbefff5e640f9f7915a064f40bb566466
Xanta <danielwaage@hotmail.de>
2019-04-10 11:16:27 +0200

    Dart nun Kommentiert

--- web/main.dart
+++ web/main.dart
@@ -4,15 +4,25 @@ import 'dart:math' as math;
 void main() async{
   //Player one = new Player(4.0, 4.0);
   //Space space_one = new Space(15.0, 15.0, one);
+  //Textbox
   var show = querySelector("#Texto");
+
+  //Vector zum testen
   Vector2D testVector = new Vector2D(0.0, 1.0);
+
+  //Winkel, wie sehr rotiert wird
   double alpha = 0.0;
-  while(true){
+  
+  //Schleife, die den Vektor einfach nur drehen sollte
+  //Funktioniert nicht ganz und zeigt "6.73678268762387628734e-16" o.Ä. dank floating point rundung.
+  //TODO Fix this RITO!
+  for(int i = 0; i < 100; i++){
     show.text = "Nun haben wir alpha (${alpha}) und sinus (${math.sin(alpha)}) und cosinus (${math.cos(alpha)})";
     alpha += math.pi / 2;
     await window.animationFrame;
   }
   /*
+  //Test für Funktion, die später das drehen erleichtert
   print("dx ist ${testVector.dx} und dy ist ${testVector.dy}");
   for(int i = 0; i < 10; i++){
 	 testVector.rotate_counter_clockwise(1);
@@ -28,7 +38,7 @@ class Space {
   Player current_player;
   Space(this.x_size_max, this.y_size_max, this.current_player);
 
-  //Function to simulate the space for every frame(?)
+  //Function to simulate the space for every frame(Kommt noch)
   void simulate() {}
 }
 
