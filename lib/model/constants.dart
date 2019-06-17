e29e20fe2296534c3f432b156120f231cc1b9ac7
Schindler'sKiste <marvin.k50@eb.de>
2019-06-17 21:58:45 +0200

    40 fps

--- lib/model/constants.dart
+++ lib/model/constants.dart
@@ -3,6 +3,6 @@
 ///tick ist die wiederholungsrate des timers
 ///bei 60 haben wir jede 60stel sekunde ein update
 ///diese kosntante wird auch für die berechnung von schüssen des gegners und des players genutzt
-final tick = 60;
+final tick = 40;
 ///growth ist zuständig für die wachstumsrate der gegner derzeit wäcsht nur der astroid
 final growth = 0.015;
